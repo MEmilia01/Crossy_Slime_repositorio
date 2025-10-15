540481e53ef0a6310d5b958e35214de0c22d2d27
MEmilia01 <mariana_cardozo@euneiz.com>
2025-10-15 16:23:12 +0100

    mas camara intentos

--- Crossy_Slime/Assets/Scripts/Objetos/Cameramove.cs
+++ Crossy_Slime/Assets/Scripts/Objetos/Cameramove.cs
@@ -2,15 +2,36 @@ using UnityEngine;
 
 public class Cameramove : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float velocidadVertical = 5f;
+    public float velocidadMovimiento = 10f;
+    public float suavizado = 0.1f;
+
+    // Componente Rigidbody para mover el objeto con física
+    public Rigidbody rb;
+
+    // Variable para guardar la velocidad actual
+    private Vector3 velocidadActual;
+
     void Start()
     {
-        
+        // Obtener el componente Rigidbody al inicio
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Obtener la entrada del usuario
+        float movimientoY = Input.GetAxis("Vertical");
+
+        Debug.Log(movimientoY);
+        // Calcular la velocidad deseada
+        Vector3 velocidadDeseada = new Vector3(0, 0, movimientoY * velocidadVertical);
+
+        // Aplicar suavizado para una transición más suave
+        velocidadActual = Vector3.Lerp(velocidadActual, velocidadDeseada, Time.deltaTime * suavizado);
+
+        // Aplicar la velocidad al Rigidbody
+        rb.linearVelocity = new Vector3(velocidadActual.x, velocidadActual.y, rb.linearVelocity.z);
+        //rb.velocity
     }
 }
