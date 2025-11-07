32354b8f9548bfc395fde36f940ccd795ff0f019
MEmilia01 <mariana_cardozo@euneiz.com>
2025-11-07 17:08:29 +0100

    Camara avanza

--- Crossy_Slime/Assets/Scripts/Objetos/Cameramove.cs
+++ Crossy_Slime/Assets/Scripts/Objetos/Cameramove.cs
@@ -1,29 +1,36 @@
 using UnityEngine;
 using DG.Tweening;
+using Unity.VisualScripting;
+using UnityEngine.InputSystem;
 
 public class Cameramove : MonoBehaviour
 {
     public GameObject player;
     public Transform targetCamera;
-
+   
+    public float distancia;
     public float speed;
     public float principio = 2;
     public float masinicio = 5;
+    public bool enrango = true;
 
     public float duration = 1f;
 
 
     void Start()
     {   
+       enrango = true;
        speed = principio;
     }
     void Update()
     {
 
         transform.position = transform.position + new Vector3(0, 0, Time.deltaTime * speed);
+       //transform.position = playerposition.position + offset;
+        
         Debug.Log("temueves, camara");
 
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.R))
         {
             Rapidez();
         }
@@ -31,12 +38,39 @@ public class Cameramove : MonoBehaviour
         {
             back();
         }
+        
+        distancia = transform.position.z - player.transform.position.z;
+        //tiene que estar entre 1 y -9
+        
+        if(distancia < -7)
+        {
+            enrango = false;
+            if(enrango != true)
+            {
+                Rapidez();
+            }
+        }
+        else
+        {
+            enrango = true;
+        }
+        
+        
 
+        /*
+        if(player.GetComponent<inputActive>)
+        {
+            //camara se queda quieta
+        }
+        */
     }  
     
     void Rapidez()
     {
         DOTween.To(() => speed, x => speed = x, masinicio, 0.2f);
+
+        if(enrango == true) { back(); }
+
     }
 
     void back()
